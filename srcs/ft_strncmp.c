479929bde2b0931b660c3c3ea62793075eb3b38f
Rouhija <sami_rouhe@hotmail.com>
2020-03-26 16:20:12 +0200

    fix to strncmp

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -12,16 +12,15 @@
 
 #include "libft.h"
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
-	unsigned char	*p1;
-	unsigned char	*p2;
+	size_t i;
 
-	p1 = (unsigned char *)s1;
-	p2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
 	i = 0;
-	while (p1[i] == p2[i] && p1[i] && p2[i] && i < n)
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return (p1[i] - p2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
+
